dcb2126d3dcf67d09f1c63abb578a5ef4f0714d6
indra037 <indrawan.saputra4@gmail.com>
2024-09-09 18:22:31 +0800

    update amg

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Diandra from './pages/Diandra'
 import Amg from './pages/Amg'
-import Home from './pages/Home'
+import Diandra from './pages/Diandra'
 
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" Component={Home} />
         <Route path="/diandra" Component={Diandra} />
-        <Route path="/amg" Component={Amg} />
+        <Route path="/" Component={Amg} />
       </Routes>
     </>
   )
